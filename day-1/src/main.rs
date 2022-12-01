eb7ff43a65a657639b6d5e895c256324de7e77c6
Kyle Bloom <kylejbloom@gmail.com>
2022-12-01 19:42:17 +0000

    feat: Day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,52 +1,54 @@
+use nom::sequence::tuple;
 #[allow(clippy::wildcard_imports)]
 use utils::*;
 
 #[derive(Debug, PartialEq, Eq)]
 struct InputData {
-    depths: Vec<i32>,
+    elfs: Vec<Vec<i64>>,
 }
 
 fn parse(input: &str) -> ParseResult<InputData> {
     use nom::{
-        character::complete::{i32, line_ending},
+        character::complete::{i64, line_ending},
         combinator::map,
         multi::separated_list1,
     };
-    let numbers = separated_list1(line_ending, i32);
-    let mut parse = map(numbers, |depths| InputData { depths });
+    let elf = separated_list1(line_ending, i64);
+    let elfs = separated_list1(tuple((line_ending, line_ending)), elf);
+    let mut parse = map(elfs, |elfs| InputData { elfs });
     parse(input)
 }
 
 #[allow(clippy::unnecessary_wraps)]
-fn part1(input: &InputData) -> AocResult<usize> {
-    Ok(input
-        .depths
-        .windows(2)
-        .filter(|a| a.first() < a.last())
-        .count())
+fn part1(input: &InputData) -> AocResult<i64> {
+    Ok(input.elfs.iter().map(|v| v.iter().sum()).max().unwrap())
 }
 
 #[allow(clippy::unnecessary_wraps)]
-fn part2(input: &InputData) -> AocResult<usize> {
-    Ok(input
-        .depths
-        .windows(4)
-        .filter(|a| a.first() < a.last())
-        .count())
+fn part2(input: &InputData) -> AocResult<i64> {
+    let mut sorted_sums: Vec<i64> = input.elfs.iter().map(|v| v.iter().sum()).collect();
+    sorted_sums.sort_by(|a, b| b.cmp(a));
+    Ok(sorted_sums.iter().take(3).sum())
 }
 
 aoc_main!(parse, part1, part2);
 
 #[test]
 fn test() {
-    let input = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
+    let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
     assert_parser!(
         parse,
         &input,
         InputData {
-            depths: vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+            elfs: vec![
+                vec![1000, 2000, 3000],
+                vec![4000],
+                vec![5000, 6000],
+                vec![7000, 8000, 9000],
+                vec![10_000]
+            ]
         }
     );
-    assert_part!(parse, part1, input, 7);
-    assert_part!(parse, part2, input, 5);
+    assert_part!(parse, part1, input, 24_000);
+    assert_part!(parse, part2, input, 45_000);
 }
